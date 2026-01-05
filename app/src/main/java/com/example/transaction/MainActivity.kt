fe9e8995e0c3597e73fbe9457135853d85ed7f64
Ishan <regmii629@gmail.com>
2026-01-05 07:11:27 +0545

    maiu menu pg made

--- app/src/main/java/com/example/transaction/MainActivity.kt
+++ app/src/main/java/com/example/transaction/MainActivity.kt
@@ -3,14 +3,15 @@ package com.example.transaction
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.transaction.salaryInput.SalaryInputActivity
+import com.example.transaction.mainMenu.MainMenuActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Launch the Salary Input screen directly
-        startActivity(Intent(this, SalaryInputActivity::class.java))
-        finish() // Optional: close MainActivity if you don't need it
+        // Launch Main Menu screen
+        startActivity(Intent(this, MainMenuActivity::class.java))
+        finish() // Close MainActivity so user can't return here
     }
 }
---
+++ app/src/main/java/com/example/transaction/mainMenu/MainMenuViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.transaction.mainMenu
+
+import androidx.lifecycle.ViewModel
+
+class MainMenuViewModel : ViewModel() {
+    // UI-only for now
+    // Navigation & logic will come later
+}
