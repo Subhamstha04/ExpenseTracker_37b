5761092b5e0cd752e60f93d57aa1a05d5d50db47
yuvarajchy <yuvarajchy123@gmail.com>
2026-01-30 20:20:02 +0545

    updated

--- app/src/main/java/com/example/transaction/register/RegisterScreen.kt
+++ app/src/main/java/com/example/transaction/register/RegisterScreen.kt
@@ -55,7 +55,7 @@ fun RegisterScreen(
         OutlinedTextField(
             value = email,
             onValueChange = { onClearError(); onEmailChange(it) },
-            label = { Text("email") },
+            label = { Text("Email") },
             modifier = Modifier.fillMaxWidth()
         )
 
